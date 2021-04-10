65bca35ace8113c2ad6c14b5b1b929b815a6b8a7
Saif <razasaif171@gmail.com>
2021-04-10 16:32:31 +0500

    f

--- assets/js/home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  // Shipment Type Selection
-  $(".shipment-type .shipment-type-selection").on("click", function () {
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-  });
-});
